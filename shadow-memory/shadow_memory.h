93865280caf1a193e0640c5a0c5b4121c758bea8
toffeenutt <99hshan@gmail.com>
2024-10-24 22:57:42 +0900

    ready for test

---
+++ shadow-memory/shadow_memory.h
@@ -0,0 +1,25 @@
+//shadow_memory.h
+
+#ifndef SHADOW_MEMORY_H
+#define SHADOW_MEMORY_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+void allocate_shadow_memory();
+
+void free_shadow_memory();
+
+static inline int8_t* get_shadow_address(void* addr);
+
+static inline size_t get_shadow_block_offset(void* addr);
+
+static inline size_t get_shadow_size(size_t size);
+
+void* wrapper_malloc(size_t size);
+
+void wrapper_free(void* addr, size_t size);
+
+void validate_memory_access(void* addr, size_t size);
+
+#endif
