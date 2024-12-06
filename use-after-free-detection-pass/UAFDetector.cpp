bc72780a2d30fea4d2248fe368ed455df4143884
toffeenutt <99hshan@gmail.com>
2024-12-07 04:23:58 +0900

    after_malloc 삽입 완료

--- use-after-free-detection-pass/UAFDetector.cpp
+++ use-after-free-detection-pass/UAFDetector.cpp
@@ -15,11 +15,16 @@ namespace {
 //main함수에 allocate_shadow_memory 함수 삽입하기 위한 Pass
 struct InsertShadowMemoryAllocation : public PassInfoMixin<InsertShadowMemoryAllocation> {
     PreservedAnalyses run(Module &M, ModuleAnalysisManager &) {
+        
+        LLVMContext &Context = M.getContext();
 
-        // allocate_shadow_memory 함수 선언 가져오기
+        //--------------------------------------------------------------------------------------------------
+        // main함수에 allocate_shadow_memory 함수 호출 삽입
+
+        // allocate_shadow_memory 함수 시그니처
         FunctionCallee allocate_shadow_memory = M.getOrInsertFunction(
             "allocate_shadow_memory",
-            FunctionType::get(Type::getVoidTy(M.getContext()), false)
+            FunctionType::get(Type::getVoidTy(Context), false)
         );
 
         // main 함수 찾기
@@ -43,6 +48,91 @@ struct InsertShadowMemoryAllocation : public PassInfoMixin<InsertShadowMemoryAll
 
         // allocate_shadow_memory() 호출 생성
         builder.CreateCall(allocate_shadow_memory);
+
+
+        //--------------------------------------------------------------------------------------------------
+        // malloc, aligned_alloc, @_Znwm (new), @_Znam (new[]) 이후에 after_malloc 호출 삽입
+
+        // after_malloc 함수 시그니처
+        FunctionCallee after_malloc = M.getOrInsertFunction(
+            "after_malloc",
+            FunctionType::get(Type::getVoidTy(Context),
+                              {Type::getInt8PtrTy(Context), Type::getInt64Ty(Context)},
+                              false)
+        );
+
+        std::vector<std::string> allocFuncNames = {
+            "malloc",
+            "aligned_alloc",
+            "_Znwm", // operator new
+            "_Znam"  // operator new[]
+        };
+
+        for (Function &F : M) {
+
+            // 함수가 선언만 되어있는 경우 스킵(정의가 아닌)
+            if (F.isDeclaration()) continue;
+
+            for (BasicBlock &BB : F) {
+                for (Instruction &I : BB) {
+                    // CallInst인지 확인
+                    if (auto *call = dyn_cast<CallInst>(&I)) {
+                        Function *calledFunc = call->getCalledFunction();
+
+                        //간접호출인 경우 무시(함수포인터 등)
+                        if (!calledFunc) continue;
+
+                        std::string funcName = calledFunc->getName().str();
+
+                        // 감지된 함수 이름인지 확인
+                        if (std::find(allocFuncNames.begin(), allocFuncNames.end(), funcName) != allocFuncNames.end()) {
+
+                            // 할당된 주소 (malloc류 함수의 리턴값)
+                            Value *addr = call;
+
+                            // 할당 크기
+                            Value *size = nullptr;
+
+                            if (funcName == "malloc" || funcName == "_Znwm" || funcName == "_Znam") {
+                                size = call->getArgOperand(0);
+                            } else if (funcName == "aligned_alloc") {
+                                size = call->getArgOperand(1);
+                            }
+
+                            // after_malloc(addr, size) 호출 생성
+                            IRBuilder<> afterBuilder(call->getNextNode());
+                            afterBuilder.CreateCall(after_malloc, {addr, size});
+                        }
+                    } //InvokeInst인지 확인
+                    else if (auto *invoke = dyn_cast<InvokeInst>(&I)) {
+                        Function *calledFunc = invoke->getCalledFunction();
+                        if (!calledFunc) continue;
+
+                        std::string funcName = calledFunc->getName().str();
+
+                        if (std::find(allocFuncNames.begin(), allocFuncNames.end(), funcName) != allocFuncNames.end()) {
+
+                            // 할당된 주소 (malloc류 함수의 리턴값)
+                            Value *addr = invoke;
+
+                            // 할당 크기
+                            Value *size = nullptr;
+
+                            if (funcName == "malloc" || funcName == "_Znwm" || funcName == "_Znam") {
+                                size = invoke->getArgOperand(0);
+                            } else if (funcName == "aligned_alloc") {
+                                size = invoke->getArgOperand(1);
+                            }
+
+                            // after_malloc(addr, size) 호출 생성
+                            BasicBlock *normalDest = invoke->getNormalDest();
+                            IRBuilder<> afterBuilder(&*normalDest->getFirstInsertionPt());
+                            afterBuilder.CreateCall(after_malloc, {addr, size});
+                        }                        
+                    }
+                }
+            }
+        }
 
         return PreservedAnalyses::none();
     }
